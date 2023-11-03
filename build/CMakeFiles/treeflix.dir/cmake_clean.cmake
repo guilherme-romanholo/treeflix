365a9605ce776933033df25ec8c274ed2ac2d346
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2023-11-03 17:54:37 -0300

    start

---
+++ build/CMakeFiles/treeflix.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/treeflix.dir/src/main.c.o"
+  "CMakeFiles/treeflix.dir/src/main.c.o.d"
+  "CMakeFiles/treeflix.dir/src/teste.c.o"
+  "CMakeFiles/treeflix.dir/src/teste.c.o.d"
+  "treeflix"
+  "treeflix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/treeflix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
