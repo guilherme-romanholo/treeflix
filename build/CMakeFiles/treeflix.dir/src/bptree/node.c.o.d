4a876f3d36e67d852b02835edd43bb45a3f30c83
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2023-11-17 17:29:33 -0300

    menu inicial

--- build/CMakeFiles/treeflix.dir/src/bptree/node.c.o.d
+++ build/CMakeFiles/treeflix.dir/src/bptree/node.c.o.d
@@ -20,9 +20,7 @@ CMakeFiles/treeflix.dir/src/bptree/node.c.o: \
  /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
  /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
  /usr/include/bits/types/cookie_io_functions_t.h \
- /usr/include/bits/stdio_lim.h /usr/include/string.h \
- /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
- /usr/include/strings.h /usr/include/stdlib.h \
+ /usr/include/bits/stdio_lim.h /usr/include/stdlib.h \
  /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
  /usr/include/sys/types.h /usr/include/bits/types/clock_t.h \
  /usr/include/bits/types/clockid_t.h /usr/include/bits/types/time_t.h \
@@ -38,4 +36,6 @@ CMakeFiles/treeflix.dir/src/bptree/node.c.o: \
  /usr/include/bits/pthreadtypes-arch.h \
  /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
  /usr/include/bits/struct_rwlock.h /usr/include/alloca.h \
- /usr/include/bits/stdlib-float.h
+ /usr/include/bits/stdlib-float.h /usr/include/string.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/strings.h
